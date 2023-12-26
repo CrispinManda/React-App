875730e647e9fc583fc01c1833221a9f01cb0dd2
Crispin Manda <crispinmanda06@gmail.com.com>
2023-12-26 14:14:18 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+      <h1 className="display-4">Hello, World!</h1>
+      <h1 className="lead">This is a simple React app with Bootstrap styling.</h1>
     </div>
   );
 }
